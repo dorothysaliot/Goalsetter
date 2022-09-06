a5a506fcc61a3bb5821982b59f7633133c5cea18
Dor Saliot <rjdsaliot@gmail.com>
2022-09-06 22:52:10 +0800

    db connection

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
+const connectDB = require('./config/db')
 const {errorHandler} = require('./middleware/errorMiddleware');
+
+connectDB();
+
 const app = express();
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
