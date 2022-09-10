4e3b7fdb986967a4b5650b24275656e873712c16
Dor Saliot <rjdsaliot@gmail.com>
2022-09-11 06:49:15 +0800

    register user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,23 +1,62 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const asyncHandler = require('express-async-handler');
+const User = require('../models/userModel');
+const { create } = require('../models/userModel');
+
 //@desc REGISTER new users
 //@route POST /api/users
 //@access PUBLIC
-const registerUser  = (req,res) =>{
-    res.send({message:'Register User'});
-}
+const registerUser  = asyncHandler(async(req,res) =>{
+    //check if the fileds are empty
+    const {name,email,password} = req.body;
+    if(!name||!email||!password){
+        res.status(400)
+        throw new Error('Please fill all fields')
+    }
+    //check if the user exist in the db
+    const userExist = await User.findOne({email});
+    if(userExist){
+        res.status(400)
+        throw new Error('User already exist');
+    }
+    //hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password,salt);
+
+    //create user
+    const user = await User.create({
+        name,
+        email,
+        password:hashedPassword
+    })
+
+    //validate if the user was created
+    if(user){
+        res.status(200).json({
+            _id:user.id,
+            name:user.name,
+            email:user.email
+        })
+    }else{
+        res.status(400)
+        throw new Error('Invalid user');
+    }
+})
 
 //@desc AUTHENTICATE users
 //@route POST /api/users/login
 //@access PUBLIC
-const loginUser  = (req,res) =>{
+const loginUser  = asyncHandler(async(req,res) =>{
     res.send({message:'Login User'});
-}
+})
 
 //@desc GET user data
 //@route GET /api/users/me
 //@access PUBLIC
-const getMe  = (req,res) =>{
+const getMe  = asyncHandler(async(req,res) =>{
     res.send({message:'User data display'});
-}
+})
 
 
 
