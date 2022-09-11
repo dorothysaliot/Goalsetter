5b355e09252d6bcc1ca4e9649107605289e8c822
Dor Saliot <rjdsaliot@gmail.com>
2022-09-11 14:21:56 +0800

    authenticate users

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -48,6 +48,21 @@ const registerUser  = asyncHandler(async(req,res) =>{
 //@route POST /api/users/login
 //@access PUBLIC
 const loginUser  = asyncHandler(async(req,res) =>{
+    const {email,password} = req.body;
+    //check for user email
+    const user = await User.findOne({email});
+
+    if(user && (await bcrypt.compare(password,user.password))){
+        res.status(200).json({
+            _id:user.id,
+            name:user.name,
+            email:user.email
+        })
+    }else{
+        res.status(400)
+        throw new Error('Invalid credentials');
+    }
+
     res.send({message:'Login User'});
 })
 
