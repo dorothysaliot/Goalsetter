862524ca1a0a1cf6149da0781a5df8da02991423
dorothysaliot <rjdlsaliot@usep.edu.ph>
2022-09-28 15:58:00 +0800

    added goals

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import {useEffect} from 'react'
 import {useNavigate} from 'react-router-dom'
 import {useSelector} from 'react-redux'
+import GoalForm from '../components/GoalForm'
 
 function Dashboard() {
 
@@ -17,8 +18,16 @@ function Dashboard() {
 
 
   return (
-    <div>Dashboard</div>
+    <>
+      <section className="heading">
+        <h1>Welcome {user && user.name} </h1>
+        <p>Goals Dashboard</p>
+      </section>
+
+      <GoalForm />
+    </>
   )
+
 }
 
 export default Dashboard
