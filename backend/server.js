40f73635cf2843198b17a6a206b7a48faf144a02
dorothysaliot <rjdlsaliot@usep.edu.ph>
2022-10-02 00:27:55 +0800

    deploy

--- backend/server.js
+++ backend/server.js
@@ -16,25 +16,18 @@ app.use(express.urlencoded({extended:false}));
 app.use('/api/goals', require('./routes/goalRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 
-//serve frontend
-if(process.env === 'production'){
-    app.use(express.static(path.join(__dirname, '../frontend/build')))
-
-    app.get('*', (req,res) => 
-        res.sendFile(
-            path.resolve(
-                '__dirname',
-                'frontend',
-                'build',
-                'index.html'
-            )
-        )
-    )
-}else{
-    app.get('/', (req,res) =>{
-        res.send('Set to production')
-    })
-}
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+  
+    app.get('*', (req, res) =>
+      res.sendFile(
+        path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+      )
+    );
+  } else {
+    app.get('/', (req, res) => res.send('Please set to production'));
+  }
 
 app.use(errorHandler);
 
