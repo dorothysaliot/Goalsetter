37c71c301149d8ad6453eb6d56c59836e5e863dc
dorothysaliot <rjdlsaliot@usep.edu.ph>
2022-10-01 21:27:15 +0800

    get goals

--- frontend/src/features/goals/goalService.js
+++ frontend/src/features/goals/goalService.js
@@ -2,20 +2,37 @@ import axios from 'axios'
 
 const API_URL = '/api/goals/'
 
-//create new goal function
-const createGoal = async(goalData,token) =>{
+// Create new goal
+const createGoal = async (goalData, token) => {
     const config = {
-        header:{
-            Authorization: `Bearer {$token}`
-        }
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
     }
-    const response = await axios.post(API_URL,goalData,token)
-
+  
+    const response = await axios.post(API_URL, goalData, config)
+  
     return response.data
-}
+  }
+
+// Get user goals
+const getGoals = async (token) => {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  
+    const response = await axios.get(API_URL, config)
+  
+    return response.data
+  }
+
+
 
 const goalService = {
-    createGoal
+    createGoal,
+    getGoals
 }
 
 export default goalService;
--- frontend/src/features/goals/goalSlice.js
+++ frontend/src/features/goals/goalSlice.js
@@ -9,20 +9,46 @@ const initialState ={
     message:''
 }
 
-//create new goal
-export const createGoal = createAsyncThunk('goals/create', async (goalData, thunkAPI) =>{
-    try {
+// Create new goal
+export const createGoal = createAsyncThunk(
+    'goals/create',
+    async (goalData, thunkAPI) => {
+      try {
         const token = thunkAPI.getState().auth.user.token
-        return await goalService.createGoal(goalData,token)
+        return await goalService.createGoal(goalData, token)
       } catch (error) {
         const message =
-          (error.response && error.response.data && error.response.data.message) ||
+          (error.response &&
+            error.response.data &&
+            error.response.data.message) ||
           error.message ||
           error.toString()
         return thunkAPI.rejectWithValue(message)
       }
-})
+    }
+  )
 
+
+
+
+// Get user goals
+export const getGoals = createAsyncThunk(
+    'goals/getAll',
+    async (_, thunkAPI) => {
+      try {
+        const token = thunkAPI.getState().auth.user.token
+        return await goalService.getGoals(token)
+      } catch (error) {
+        const message =
+          (error.response &&
+            error.response.data &&
+            error.response.data.message) ||
+          error.message ||
+          error.toString()
+        return thunkAPI.rejectWithValue(message)
+      }
+    }
+  )
 
 
 //actual slice
@@ -43,6 +69,19 @@ export const goalSlice = createSlice({
                 state.goals.push(action.payload)
             })
             .addCase(createGoal.rejected, (state,action) =>{
+                state.isLoading = false
+                state.isError = true
+                state.message = action.payload
+            })
+            .addCase(getGoals.pending, (state)=>{
+                state.isLoading = true
+            })
+            .addCase(getGoals.fulfilled, (state,action) =>{
+                state.isLoading = false
+                state.isSuccess = true
+                state.goals = action.payload
+            })
+            .addCase(getGoals.rejected, (state,action) =>{
                 state.isLoading = false
                 state.isError = true
                 state.message = action.payload
