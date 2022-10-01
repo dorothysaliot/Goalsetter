b9c6cd5bb0de3a080ec1090e72d95ca934e0c23e
dorothysaliot <rjdlsaliot@usep.edu.ph>
2022-10-02 00:13:06 +0800

    deploy

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -14,6 +15,26 @@ app.use(express.urlencoded({extended:false}));
 
 app.use('/api/goals', require('./routes/goalRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
+
+//serve frontend
+if(process.env === 'production'){
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    app.get('*', (req,res) => 
+        res.sendFile(
+            path.resolve(
+                '__dirname',
+                'frontend',
+                'build',
+                'index.html'
+            )
+        )
+    )
+}else{
+    app.get('/', (req,res) =>{
+        res.send('Set to production')
+    })
+}
 
 app.use(errorHandler);
 
